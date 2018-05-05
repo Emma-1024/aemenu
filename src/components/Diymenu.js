60102b3130bff484a56271e5788ffdb16f0b2e34
Emma <emma.wu.1024@gmail.com>
2018-05-05 18:25:08 +0800

    Add function for Diymenu component

--- src/components/Diymenu.js
+++ src/components/Diymenu.js
@@ -7,48 +7,54 @@ class Diymenu extends Component {
     super()
     this.state = {
       showForm: false,
+      showBasket: false,
       dish: '',
       ingredientName: '',
       ingredientNum: '',
-      ingredientList:[],
+      ingredientList: [],
     }
-    this.basketListObj={}
+    this.basketListObj = {}
   }
   render() {
     return (
       <div>
         <div>
-          <Button onClick={ ()=> this._toggleShow() }>点击添加菜单</Button>
-          { this.state.showForm ?
-          <div className="formlist">
-            <form action="#" id="menuform">
-              <label htmlFor="dishes">菜名:
-                <input type="text" id="dishes" placeholder="请输入一个菜名" value={ this.state.dish }
-                  onChange={ (e) => this._handleDish(e) }/>
-              </label>
-              <label htmlFor="ingredient">食材:
-                <input type="text" name="ingredient" placeholder="请输入食材" value={ this.state.ingredientName }
-                onChange={ (e) => this._handleIngredientName(e) }/>
-              </label>
-              <label htmlFor="num">数量:
-                <input type="text" name="num" placeholder="请输入数量" value={ this.state.ingredientNum }
-                onChange={ (e) => this._handleIngredientNum(e) }/>
-              </label>
-              <ButtonToolbar>
-                <OverlayTrigger placement="right" overlay={tooltip}>
-                  <Button className="glyphicon glyphicon-plus" onClick={ ()=> this._addList() }>添加到列表</Button>
-                </OverlayTrigger>
-              </ButtonToolbar>
-              <div className="inputlist">
-                { this._showList() }
-              </div>
-              <Button bsStyle="success" onClick={ this._add2basket.bind(this) }>完成->加入菜篮</Button>
-            </form>
-          </div>
-          : null }
+          <Button onClick={() => this._toggleShow()}>点击添加菜单</Button>
+          {this.state.showForm ?
+            <div className="formlist">
+              <form action="#" id="menuform">
+                <label htmlFor="dishes">菜名:
+                <input type="text" id="dishes" placeholder="请输入一个菜名" value={this.state.dish}
+                    onChange={(e) => this._handleDish(e)} />
+                </label>
+                <label htmlFor="ingredient">食材:
+                <input type="text" name="ingredient" placeholder="请输入食材" value={this.state.ingredientName}
+                    onChange={(e) => this._handleIngredientName(e)} />
+                </label>
+                <label htmlFor="num">数量:
+                <input type="text" name="num" placeholder="请输入数量" value={this.state.ingredientNum}
+                    onChange={(e) => this._handleIngredientNum(e)} />
+                </label>
+                <ButtonToolbar>
+                  <OverlayTrigger placement="right" overlay={tooltip}>
+                    <Button className="glyphicon glyphicon-plus" onClick={() => this._addList()}>添加到列表</Button>
+                  </OverlayTrigger>
+                </ButtonToolbar>
+                <div className="inputlist">
+                  {this._showList()}
+                </div>
+                <Button bsStyle="success" onClick={this._add2basket.bind(this)}>完成->加入菜篮</Button>
+              </form>
+            </div>
+            : null}
         </div>
         <div className="basket">
-          <Button bsStyle="info" onClick={ this._checkBasket.bind(this) }>查看菜篮</Button>
+          <Button bsStyle="info" onClick={this._checkBasket.bind(this)}>查看菜篮</Button>
+          {this.state.showBasket ?
+            <div>
+              <ShowBasketList data={this.basketListObj}></ShowBasketList>
+            </div>
+            : null}
         </div>
       </div>
     )
@@ -74,7 +80,7 @@ class Diymenu extends Component {
     })
   }
   _addList() {
-    if(this.state.ingredientName.trim() === '' || this.state.ingredientNum.trim() === ''){
+    if (this.state.ingredientName.trim() === '' || this.state.ingredientNum.trim() === '') {
       return
     }
     this.state.ingredientList.push({ ingredientName: this.state.ingredientName, ingredientNum: this.state.ingredientNum })
@@ -82,9 +88,9 @@ class Diymenu extends Component {
   }
   _showList() {
     var arr = []
-    this.state.ingredientList.forEach((item,index) => {
-      arr.push(<ListItem key={ index } name={ item.ingredientName } num={ item.ingredientNum }
-        callback={ () => this._removeItem(index) }/>)
+    this.state.ingredientList.forEach((item, index) => {
+      arr.push(<ListItem key={index} name={item.ingredientName} num={item.ingredientNum}
+        callback={() => this._removeItem(index)} />)
     })
     return arr
   }
@@ -95,10 +101,10 @@ class Diymenu extends Component {
     })
   }
   _add2basket() {
-    if(this.basketListObj.hasOwnProperty(this.state.dish)){
-      this.basketListObj[this.state.dish].push({ingredientName: this.state.ingredientName, ingredientNum: this.state.ingredientNum})
-    }else{
-      this.basketListObj[this.state.dish]=this.state.ingredientList
+    if (this.basketListObj.hasOwnProperty(this.state.dish)) {
+      this.basketListObj[this.state.dish].push({ ingredientName: this.state.ingredientName, ingredientNum: this.state.ingredientNum })
+    } else {
+      this.basketListObj[this.state.dish] = this.state.ingredientList
     }
     this.setState({
       ingredientList: []
@@ -110,6 +116,9 @@ class Diymenu extends Component {
     // console.log(this.basketListObj)
   }
   _checkBasket() {
+    this.setState({
+      showBasket: !this.state.showBasket
+    })
     console.log(this.basketListObj)
   }
 }
@@ -123,10 +132,40 @@ class ListItem extends Component {
   render() {
     return (
       <div className="listbox">
-        <span className="listitem">{ this.props.name }:{ this.props.num }</span>
-        <span className="glyphicon glyphicon-remove" onClick={ this.props.callback }></span>
+        <span className="listitem">{this.props.name}:{this.props.num}</span>
+        <span className="glyphicon glyphicon-remove" onClick={this.props.callback}></span>
       </div>
     )
   }
 }
-export default  Diymenu
+
+class ShowBasketList extends Component {
+  render() {
+    return (
+      <div>
+        {this._renderBasket(this.props.data)}
+      </div>
+    )
+  }
+  _renderBasket(data) {
+    var newArr = []
+    var i = 0
+    for (var k in data) {
+      var arr = []
+      arr.push(<div className="dishName" key={i++}>{k}</div>)
+      arr.push(data[k].map((item, index) => {
+        return (
+          <div className="ingredientBox" key={index}>
+            <span className="ingredientStyle">{ item.ingredientName }</span>
+            <span className="ingredientStyle">{ item.ingredientNum }</span>
+          </div>
+          )
+      }))
+      newArr.push(<div key={i++} className="listItem">{ [arr] }</div>)
+    }
+
+    return newArr
+
+  }
+}
+export default Diymenu
