51e4e9997da164eb31b642be03ab31fc5de9ea8a
Emma <emma.wu.1024@gmail.com>
2018-05-22 15:53:11 +0800

    Modify footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,24 +3,30 @@ import { Grid, Row, Col } from 'react-bootstrap'
 import ScrollTopButton from './ScrollTopButton'
 // import Pagination from '../components/PaginationComp'
 
-import '../css/footer.css';
+import '../css/footer.css'
+import recruitMe from '../images/img/Spring-me-icon1.png'
 
 function Footer () {
     return (
       <div>
         <Grid fluid>
           <Row className="footer">
-            <Col xs={4}>
+            <Col xs={3}>
               <h4>About us</h4>
             </Col>
-            <Col xs={8}>
-              <div className="link pull-left">
+            <Col xs={4}>
+              <div className="link">
                 {/* <a href="https://github.com/Emma-1024">GitHub of Emma</a><br />
                 <a href="https://github.com/Alex-T-1024">GitHub of Alex</a><br /> */}
-                <a href="https://github.com/Emma-1024/aemenu.git">Project GitHub</a><br/>
-                <a href="https://github.com/Alex-T-1024/aemenu-backend">Project GitHub</a>
+                <a href="https://github.com/Emma-1024/aemenu.git">Front end</a><br/>
+                <a href="https://github.com/Alex-T-1024/aemenu-backend">Back end</a>
               </div>
-              <div className="recruit pull-right">recruit me</div>
+            </Col>
+            <Col xs={5}>
+              <div className="recruit">
+                <img src={ recruitMe } alt=""/>
+                <a href="/">recruit me</a>
+              </div>
             </Col>
           </Row>
         </Grid>
